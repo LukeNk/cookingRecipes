1bf7aa927f0d02eed46eaf568163f68c90478c18
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-06-17 08:21:11 +0700

    Added basic structure

---
+++ data/id.js
@@ -0,0 +1,17 @@
+const ingredientTypes = [
+    'Vegetable',
+    'Protein',
+    'Fruit',
+    'Grain',
+    'Seasoning',
+    'Baking',
+    'Diary'
+];
+
+const ingredients = [
+    { name: 'Onion', unit: 'g', type: 0 },
+    { name: 'Garlic', unit: 'g', type: 0 },
+    { name: 'Tomato', unit: '', type: 0 },
+    { name: 'Potato', unit: '', type: 0 },
+    { name: 'Egg', unit: '', type: 1 },
+];
---
+++ data/recipes.js
@@ -0,0 +1,11 @@
+const recipes = [
+    {
+        name: 'Boiled egg',
+        servings: 1,
+        prepTime: 1,
+        cookTime: 8,
+        ingredients: [
+            { id: 4, quantity: 1 }
+        ]
+    }
+]
