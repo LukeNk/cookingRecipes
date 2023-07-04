b5f6cf1ddd97dea18541cd36a361532fab303dc5
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-07-04 20:15:05 +0700

    Added new recipe

--- data/id.js
+++ data/id.js
@@ -9,9 +9,14 @@ const ingredientTypes = [
 ];
 
 let ingredients = [
-    { name: 'Onion', unit: 'g', type: 0 },
-    { name: 'Garlic', unit: 'g', type: 0 },
+    { name: 'Onion', unit: '', type: 0 },
+    { name: 'Garlic', unit: '', type: 0 },
     { name: 'Tomato', unit: '', type: 0 },
     { name: 'Potato', unit: '', type: 0 },
     { name: 'Egg', unit: '', type: 1 },
+    { name: 'Beef (ground)', unit: 'g', type: 1 },
+    { name: 'Carrot', unit: '', type: 0 },
+    { name: 'Ketchup', unit: 'spoon', type: 4 },
+    { name: 'Tomato sauce', unit: 'ml', type: 4 },
+    { name: 'Milk (no sugar)', unit: 'ml', type: 6 }
 ];
--- data/recipes.js
+++ data/recipes.js
@@ -7,5 +7,21 @@ const recipes = [
         ingredients: [
             { id: 4, quantity: 1 }
         ]
+    }, 
+    {
+        name: 'Bolognese sauce (by Peter)',
+        prepTime: 60,
+        cookTime: 180,
+        servings: 8,
+        ingredients: [
+            { id: 5, quantity: 1 },
+            { id: 2, quantity: '1 kg' },
+            { id: 6, quantity: 1 },
+            { id: 0, quantity: 0.5 },
+            { id: 1, quantity: '3 cloves' },
+            { id: 7, quantity: 6 },
+            { id: 8, quantity: 300 },
+            { id: 9, quantity: 300 },
+        ]
     }
 ]
